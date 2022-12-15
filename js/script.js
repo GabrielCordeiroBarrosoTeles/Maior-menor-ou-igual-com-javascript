d6bd0e2974f5b581b9d7c5abfbe2f8b9528cb080
Gabriel Cordeiro Barroso Teles <98492418+GabrielCordeiroBarrosoTeles@users.noreply.github.com>
2022-12-15 11:26:34 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@ function imprimir(){
     var v1=Number(document.getElementById('v1').value)
     var v2=Number(document.getElementById('v2').value)
     if(v1>v2){  
-        resultado =`Valor 1 que é (${v1}), é maior que valor 2 (${v2})`
+        resultado =`${v1} é maior que ${v2}`
     }else if(v1<v2){
-        resultado=`Valor 1 que é (${v1}), é menor que valor 2 (${v2})`
+        resultado=`${v1} é menor que ${v2}`
     }else if(v1==v2){
-        resultado=`Valor 1 que é (${v1}), é igual a valor 2 que é (${v2}) também`
+        resultado=`${v1} é igual a ${v2}`
     }
     document.getElementById('resultado').value=resultado
 }
